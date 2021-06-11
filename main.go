32bcb717fb2d4c59bc1ac6c62f4b4a92a9e0217e
Nosa Omorodion <japhet.omorodion@.com>
2021-06-11 03:56:08 +0100

    Redundant codes removed, restructured files

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"log"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
-	"github.com/hashicorp/terraform-provider-scaffolding/internal/provider"
 )
 
 // Run "go generate" to format example terraform files and generate the docs for the registry/website
